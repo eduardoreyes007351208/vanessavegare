11da30861fe6347c3fa19d02bfff5000dfd2636d
eduardoreyes007351208 <lalosoccer307@gmail.com>
2024-09-18 11:04:10 -0700

    phone number

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -21,7 +21,7 @@
     <div class="headerWords">
       <h2 class="headerText">Vanessa Vega</h2>
       <p class="headerText">DRE 02227468</p>
-      <p class="headerText">(909)746-9872</p>
+      <a href="tel:+1-909-746-9872" class="headerText">(909)746-9872</a>
     </div>
 
     <a href="#0;" id="hamDiv" on:click={hamburgerFunc}>
